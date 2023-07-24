3f35fcce197b4a493492c3f460188665c05b6105
Matty <mattheas@ualberta.ca>
2023-07-24 12:53:52 -0600

    Referencing Issue #4. DocString updated, duplicating what Devin said in his EPS simulated subsystem doc string, however I added an important note about how to run and interact with the python script in a Windows environment, i.e., using netcat (nc) through MobaXTerm as it is not built in to the Windows PowerShell

--- ADCS/adcs_subsytem.py
+++ ADCS/adcs_subsytem.py
@@ -1,4 +1,46 @@
-"""Empty Doc String to conform to Pylint"""
+"""This python program represents a simulated version of the ADCS subsystem component for ExAlta3.
+
+For now the sub system communicates with strings over a TCP socket. The strings are parsed into a 
+command type and associated data.
+
+Until we know more system specs I am assuming there are three types of commands that can be sent: 
+    - Request - Request a paramater from the state dictionary
+    - Update  - Update a parameter in the state dictionary
+    - Execute - Execute a command (e.g. reset watchdog timer, deploy magnetometer boom, save img, save config, initiate XXXXXX, etc)
+
+# Example to update the WatchdogResetTime:
+     update:WatchdogResetTime:48.0
+
+# Example to request the Voltage:
+    request:Voltage
+
+# Example to execute a ResetWatchdogTimer command:
+    execute:ResetWatchdogTimer
+
+For now you can test your commands using netcat (nc) from the command line, and piping the command 
+to the socket from a seperate text file. 
+
+
+
+*************************************************************FOR WINDOWS USERS*************************************************************
+
+I do not believe the netcat (nc) command exists in the Windows Powershell. What I used was MobaXTerm as
+I had it installed from a previous course AND it comes with the netcat (nc) command.
+
+There are then two steps required to run this program and send commands to it:
+
+1. Run the python script, I call it from Windows Powershell with this command "python .\eps_subsystem.py"
+
+2. Use the following command in the MobaXTerm terminal to connect to the socket created in the python script "nc [DEFAULT_HOST] [DEFAULT_PORT]" (e.g., nc 127.0.0.1 1801)
+
+*It is important to note that if you give an argument to the python script, it creates a socket using that argument as the port number, so then when calling the 
+netcat command, you must use that same argument
+
+From there you simply type your commands into the MobaXTerm terminal (e.g., update:WatchdogResetTime:5.0), and hit enter to send the command to the .py script
+
+
+"""
+
 import sys
 import socket
 

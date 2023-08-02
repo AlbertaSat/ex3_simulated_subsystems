36214b62caa2e671c840c87551289fce73786db1
Abhishek Naik <abhisheknaik22296@gmail.com>
2023-08-01 18:00:45 -0600

    revamped the code flow for the python subsystem

--- IRIS/iris_subsystem.py
+++ IRIS/iris_subsystem.py
@@ -3,7 +3,7 @@
 For now the sub system communicates with strings over a TCP socket. The strings are parsed into a
 command type and associated data.
 
-Until we know more system specs I am assuming there are three types of commands that can be sent:
+Until we know more system specs we assume there are three types of commands that can be sent:
     - Request - Request a paramater from the state dictionary
     - Update  - Update a parameter in the state dictionary
     - Execute - Execute a command (e.g. reset watchdog timer)
@@ -33,113 +33,119 @@ DEFAULT_HOST = '127.0.0.1'
 DEFAULT_PORT = 1801
 COMMAND_DELIMITER = ':'
 
-default_iris_state = {
-    'PowerStatus': 1,           # 1 means powered on, 0 means off
-    'SensorStatus': 0,          # 1 means sensors are on, 0 means off
-    'TempVIS': 25,              # in degree Celsius
-    'TempNIR': 25,              # in degree Celsius
-    'TempFLASH': 25,            # in degree Celsius
-    'TempGATE': 25,             # in degree Celsius
-    'NumImages': 5,             # number of images
-    'SoftwareVersion': 1.0
-}
 
-iris_state = default_iris_state
+# Factory pattern: Command Factory
+class CommandFactory:
+    """Factory class to create command objects based on command type."""
 
+    def __init__(self, subsystem):
+        self.subsystem = subsystem
 
-class RequestDataCommand():  # pylint: disable=too-few-public-methods
-    """Execute a 'request' type command to get the current value of a parameter"""
-
-    def execute(self, params=None):
+    def command_request_data(self, params=None):
         """
+        Executes a 'request' type command to get the current value of a parameter.
         Args:
             params (list): Name of the parameter to request from the IRIS subsystem state
         Returns:
             str: A string containing the requested parameter and its associated value
         """
         print("Request command received: " + params[0])
-        if params and len(params) == 1 and params[0] in iris_state:
-            return f"{params[0]}:{iris_state[params[0]]}\n"
+        if params and len(params) == 1 and params[0] in self.subsystem.state:
+            return f"{params[0]}:{self.subsystem.state[params[0]]}\n"
         return "ERROR: Invalid request command \n"
 
-
-class UpdateParameterCommand():  # pylint: disable=too-few-public-methods
-    """Execute an 'update' type command to update a parameter value
-
-    Updateable parameters are defined in the updateable_parameters tuple.
-    """
-
-    updatable_parameters = ['PowerStatus', 'SensorStatus']
-
-    def execute(self, params=None):
+    def command_update_parameter(self, params=None):
         """
+        Executes an 'update' type command to update a parameter value.
         Args:
-            params (list): Name of the state parameter to update, and the new value
-
+            params (list): Name of the parameter to request from the IRIS subsystem state
         Returns:
-            str: A string containing the updated parameter and its new value
+            str: A string containing the requested parameter and its associated value
         """
-
         print("Update command received: " + params[0])
         if params and len(params) == 2 and params[0] in self.updatable_parameters:
-            iris_state[params[0]] = params[1]
+            self.subsystem.state[params[0]] = params[1]
             return f"Updated {params[0]} to {params[1]}\n"
         return "ERROR: update command \n"
 
-
-class ExecuteCommand():  # pylint: disable=too-few-public-methods
-    """Execute an 'execute' type command to call a function (do something)
-
-    Executable commands are defined in the executable_commands tuple.
-    """
-
-    executable_commands = ['TakeImage', 'SetTime']
-
-    def execute(self, params=None):
+    def command_execute(self, params=None):
         """
+        Executes an 'execute' type command to call a function.
         Args:
             params (list): Name of the function to call
-
         Returns:
             str: A string containing the return value from the function call
         """
-
         print("Execute command received: " + params[0])
         if params and len(params) == 1 and params[0] in self.executable_commands:
             return f"Command {params[0]} executed \n"
         return "ERROR: Invalid execute command \n"
 
-
-# Factory pattern: Command Factory
-class CommandFactory:  # pylint: disable=too-few-public-methods
-    """Factory class to create command objects based on command type"""
-
     def create_command(self, command_type):
-        """Create a command object based on the command type
+        """Creates a command object based on the command type.
         Args:
             command_type (str): The type of command to be created
-
         Returns:
-            Command: A command object of the associated command type
+            the method that was asked for in 'command_type'
         """
-
         if command_type == 'request':
-            return RequestDataCommand()
+            return self.command_request_data
         if command_type == 'update':
-            return UpdateParameterCommand()
+            return self.command_update_parameter
         if command_type == 'execute':
-            return ExecuteCommand()
+            return self.command_execute
         return None
 
 
+class IRISCommandFactory(CommandFactory):
+    """Custom command factory for IRIS-specific commands."""
+
+    def command_execute(self, params=None):
+        print("Execute command received: " + params[0])
+        if params and len(params) == 1 and params[0] in self.subsystem.executable_commands:
+            if params[0] == 'TakeImage':
+                self.subsystem.state['NumImages'] += 1
+                print('Increased NumImages by 1')
+                # ToDo: do more stuff here
+            return f"Command {params[0]} executed \n"
+        return "ERROR: Invalid execute command \n"
+
+
+class IRISSubsystem:
+    def __init__(self):
+        self.power_status = 1           # 1 means powered on, 0 means off
+        self.sensor_status = 0          # 1 means sensors are on, 0 means off
+        self.temp_vis = 25              # in degree Celsius
+        self.temp_nir = 25              # in degree Celsius
+        self.temp_flash = 25            # in degree Celsius
+        self.temp_gate = 25             # in degree Celsius
+        self.num_images = 5             # number of images
+        self.max_num_images = 20        # maximum images that can be stored
+        self.software_version = 1.0
+        self.state = {
+            'PowerStatus': self.power_status,
+            'SensorStatus': self.sensor_status,
+            'TempVIS': self.temp_vis,
+            'TempNIR': self.temp_nir,
+            'TempFLASH': self.temp_flash,
+            'TempGATE': self.temp_gate,
+            'NumImages': self.num_images,
+            'SoftwareVersion': self.software_version,
+            # ToDo: add time
+        }
+        self.updatable_parameters = ['PowerStatus', 'SensorStatus']
+        self.executable_commands = ['TakeImage', 'SetTime']
+
+
 class IRISCommandHandler:
-    """This takes a client socket arg and uses it to listen for commands"""
+    """Takes a client socket arg and uses it to listen for commands."""
 
     def __init__(self, client_socket):
         self.client_socket = client_socket
+        self.command_factory = IRISCommandFactory(IRISSubsystem())
 
     def handle_command(self):
-        """Listen for commands from the client socket and processes them"""
+        """Listens for commands from the client socket and processes them."""
         while True:
             try:
                 data = self.client_socket.recv(1024).decode().strip()
@@ -156,7 +162,7 @@ class IRISCommandHandler:
         self.client_socket.close()
 
     def parse_command(self, command):
-        """Parse the command into consituent command type, and associated data
+        """Parses the command into consituent command type, and associated data.
 
         Args:
             command (str): A decoded and stripped string containing the command
@@ -167,7 +173,7 @@ class IRISCommandHandler:
         return command.split(COMMAND_DELIMITER)
 
     def process_command(self, command):
-        """Process command data based on the command type
+        """Processes command data based on the command type.
 
         Command types are either: update, request, or execute. This function
         relies on the pattern factory to creates a command object for the
@@ -180,15 +186,13 @@ class IRISCommandHandler:
         Returns:
             str: A string containing the response to the command
         """
-
         command_type = command[0]
         params = command[1:]
 
-        command_factory = CommandFactory()
-        command_obj = command_factory.create_command(command_type)
+        command_obj = self.command_factory.create_command(command_type)
 
         if command_obj is not None:
-            response = command_obj.execute(params)
+            response = command_obj(params)
         else:
             response = "ERROR: Invalid command type \n"
 

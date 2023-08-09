9c3b267f7964e4582ee08adec45b74f48f34fe0e
Devin <headrick@ualberta.ca>
2023-08-09 14:34:47 -0600

    Modified comments and changed licenses to apache v2

--- IRIS/iris_subsystem.py
+++ IRIS/iris_subsystem.py
@@ -96,3 +96,20 @@ if __name__ == "__main__":
 
     create_socket_and_listen(host=DEFAULT_HOST, port=PORT,
                              command_handler_obj=handler)
+
+
+# pylint: disable=duplicate-code
+# no error
+__author__ = "Abhishek Naik"
+__copyright__ = """
+    Copyright (C) 2023, [Abhishek Naik]
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License."""

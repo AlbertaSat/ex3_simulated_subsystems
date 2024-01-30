1efce824fb10a81b24c7c39252c4bb9047543928
Bach <waridh@ualberta.ca>
2024-01-29 20:09:00 -0700

    put a little more work into the class

---
+++ ADCS/adcs_subsystem.py
@@ -0,0 +1,37 @@
+
+import sys
+sys.path.append("../")
+from socket_stuff import create_socket_and_listen
+from command_factory import CommandFactory
+from command_handler import CommandHandler
+
+class ADCSSubsystem:
+    """
+    This class represents the simulated ADCS subsystem.
+    """
+    def __init__(self, builder_dict):
+        """
+        Base constructor. Uses a dictionary with the default values.
+        """
+        self.state = builder_dict   # This is placeholder.
+
+
+def command_line_handler(argv):
+    """
+    Control flow for what to return depending on the commandline arg.
+
+    **Change here if you need to change the port and address values**
+
+    Returns:
+        (PORT, HOST)
+    """
+    default_host = "127.0.0.1"
+    default_port = 1802
+
+    return (argv[1] if len(argv) > 1 else default_port), default_host
+
+
+if __name__ == "__main__":
+    port, host = command_line_handler(sys.argv)
+
+    print(f"Starting ADCS subsystem on port {port}")

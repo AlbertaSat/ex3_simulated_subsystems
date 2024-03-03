0bd96b8834a4c3b96dc3d5b53e44502efc1d1beb
Ben F <bsfisher@ualberta.ca>
2024-03-02 22:06:08 -0700

    Added basic IRIS subsystem class

--- IRIS/iris_subsystem.py
+++ IRIS/iris_subsystem.py
@@ -24,23 +24,32 @@ Usage:
 Copyright 2023 [Abhishek Naik]. Licensed under the Apache License, Version 2.0
 """
 
+IRIS_COMMAND_SIZE = 1024
 
-import sys
-sys.path.append("../")
-from socket_stuff import create_socket_and_listen   # pylint: disable=C0413
-from command_handler import CommandHandler          # pylint: disable=C0413
-from command_factory import CommandFactory          # pylint: disable=C0413
-
-
-DEFAULT_HOST = '127.0.0.1'
-DEFAULT_PORT = 1821
 DEFAULT_STATE_VALUES = {                # at some point, we should simulate temperature changes
             'PowerStatus': 1,           # 1 means powered on, 0 means off
             'SensorStatus': 0,          # 1 means sensors are on, 0 means off
             'NumImages': 0,             # number of images
             'MaxNumImages': 20,         # maximum images that can be stored
-            'DateTime': '1694902684'    # arbitrary value for now (time at which this was written)
+            'DateTime': '1707677962'    # arbitrary value for now (time at which this was written)
         }
+# void get_housekeeping(housekeeping_packet_t *hk);
+# void take_image();
+# void get_image_count(uint8_t *cnt);
+# int get_image_length(uint32_t *image_length, uint8_t index);
+# void turn_off_sensors();
+# void turn_on_sensors();
+# void set_configurations(Iris_config *config);
+# int initalize_sensors();
+# int onboot_sensors(uint8_t sensor);
+# void set_rtc_time(uint32_t obc_unix_time);
+# void get_rtc_time(Iris_Timestamp *timestamp);
+# int transfer_image_to_nand(uint8_t sensor, uint8_t *file_timestamp);
+# int delete_image_file_from_queue(uint16_t index);
+# NAND_FILE *get_image_file_from_queue(uint8_t index);
+# void set_capture_timestamp(uint8_t *file_timestamp, uint8_t sensor);
+# int store_file_infos_in_buffer();
+# void flood_cam_spi();
 
 
 class IRISSubsystem: # pylint: disable=too-many-instance-attributes
@@ -63,39 +72,50 @@ class IRISSubsystem: # pylint: disable=too-many-instance-attributes
         }
         self.updatable_parameters = ['PowerStatus', 'SensorStatus', 'Time']
         self.executable_commands = {
-            'TakeImage': self.take_image,
-            'Reset': self.reset
+            # Executable commands in tuple form of command() and n_parameters to command
+            'TKI': (self.take_image, 0),
+            'RST': (self.reset, 0),
+            'FTI': (self.get_image, 1),
+            'FTH': (self.get_housekeeping, 0),
+            'STT': (self.set_time, 1)
         }
+    def get_commands(self):
+        """Returns all possible command keys of the IRIS subsystem"""
+        return self.executable_commands.keys()
 
+    # ---- IRIS Simulated Commands, all must be within self.executable_commands ----
     def take_image(self):
         """Simulates taking a picture using the IRIS camera."""
         self.state['NumImages'] += 1
-        print('Increased NumImages by 1')
+        return 'Increased NumImages by 1'
 
     def reset(self):
         """Simulates a 'factory reset' of the IRIS subsystem."""
         for key, value in DEFAULT_STATE_VALUES.items():
             self.state[key] = value         # temp is what the temp is, doesn't get reset
-        print('Factory reset performed.')
+        return 'Factory reset performed.'
 
+    def get_image(self, n_images):
+        """Simulates fecthing n_images stored on the IRIS subsystem."""
+        # print('Fetching', n_images, 'Images...')
+        #TO-DO
+        return n_images + ' images fetched'
 
-if __name__ == "__main__":
-    # If there is no arg, port is default otherwise use the arg
-    PORT = sys.argv[1] if len(sys.argv) > 1 else DEFAULT_PORT
-    print(f"Starting IRIS subsystem on port {PORT}")
+    def get_housekeeping(self):
+        """Simulates fecthing the housekeeping data on the IRIS subsystem."""
+        return self.state.items()
 
-    command_factory = CommandFactory(IRISSubsystem())
-    handler = CommandHandler(command_factory)
-
-    create_socket_and_listen(host=DEFAULT_HOST, port=PORT,
-                             command_handler_obj=handler)
+    def set_time(self, time):
+        """Simulates setting the time of the IRIS subsystem"""
+        self.state['Time'] = time
+        return 'Time updated to ' + self.state['Time']
 
 
 # pylint: disable=duplicate-code
 # no error
-__author__ = "Abhishek Naik"
+__author__ = "Ben Fisher, Abhishek Naik"
 __copyright__ = """
-    Copyright (C) 2023, [Abhishek Naik]
+    Copyright (C) 2024, [Ben Fisher, Abhishek Naik]
     Licensed under the Apache License, Version 2.0 (the "License");
     you may not use this file except in compliance with the License.
     You may obtain a copy of the License at

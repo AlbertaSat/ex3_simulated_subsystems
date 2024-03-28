f55fe22033fde0b9d903a6b705f8b42e913cdf8c
rrasmuss07 <rrasmuss@ualberta.ca>
2024-03-28 09:04:07 -0600

    Pylint on client fixed.

--- UHF/uhf_client.py
+++ UHF/uhf_client.py
@@ -1,3 +1,4 @@
+"""Module providing the socket"""
 import socket
 import pickle
 
@@ -5,10 +6,11 @@ DEFAULT_HOST = '127.0.0.1'
 DEFAULT_PORT = 8352
 
 def serialize_file(filename):
-        with open(filename, 'rb') as file:
-            file_contents = file.read()
-            serialized_data = pickle.dumps(file_contents)
-            return serialized_data
+    """Turns a text file into a binary stream to send over a socket"""
+    with open(filename, 'rb') as file:
+        file_contents = file.read()
+        serialized_data = pickle.dumps(file_contents)
+        return serialized_data
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((DEFAULT_HOST, DEFAULT_PORT))

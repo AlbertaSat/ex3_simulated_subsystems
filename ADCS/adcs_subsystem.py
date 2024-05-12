8e91428cd00469c14f944503861209fa24f95c11
waridh <waridh@proton.me>
2024-05-11 20:11:03 -0400

    tried making a better constructor

--- ADCS/adcs_subsystem.py
+++ ADCS/adcs_subsystem.py
@@ -12,6 +12,7 @@ class ADCSState(Enum):
     """
     Enum to represent the states of the ADCS
     """
+
     OFF = auto()
     WORKING = auto()
 
@@ -21,12 +22,19 @@ class ThreeDimensionalMeasurements:
     Abstract class that deals with having multiple three
     dimensional stored values
     """
+
+    @staticmethod
+    def new():
+        """Factory method test"""
+        return ThreeDimensionalMeasurements(0, 0, 0)
+
     def __init__(self, x, y, z):
         self.x = x
         self.y = y
         self.z = z
 
     def __repr__(self):
+        """This function is for debugging by printing the object"""
         return f"({self.x!r}, {self.y!r}, {self.z!r})"
 
 
@@ -49,6 +57,7 @@ class ADCSSubsystem:
     """
     This class represents the simulated ADCS subsystem.
     """
+
     def __init__(self, interface: InterfaceInterface):
         """
         Base constructor. Uses a dictionary with the default values.
@@ -61,20 +70,16 @@ class ADCSSubsystem:
         self.state = ADCSState.OFF
 
     def start(self):
-        """This method should start the simulation for the ADCS subsystem.
-        """
+        """This method should start the simulation for the ADCS subsystem."""
         print(f"Starting ADCS subsystem with interface: {self.interface!r}")
         self.interface.connect()
 
     def send_data(self):
-        """This method sends the data through the interface
-        """
+        """This method sends the data through the interface"""
         raise NotImplementedError
 
     def __repr__(self):
-        return (f"ADCSSubsystem(\n" +
-                f"{self.__dict__!r}"
-                f"\n)")
+        return f"ADCSSubsystem(\n" + f"{self.__dict__!r}" f"\n)"
 
 
 def command_line_handler(argv) -> tuple[int, str]:
@@ -90,8 +95,8 @@ def command_line_handler(argv) -> tuple[int, str]:
     default_host = "127.0.0.1"
     default_port = 1802
 
-    ret_port = (int(argv[1]) if len(argv) > 1 else default_port)
-    ret_host = (argv[2] if len(argv) > 2 else default_host)
+    ret_port = int(argv[1]) if len(argv) > 1 else default_port
+    ret_host = argv[2] if len(argv) > 2 else default_host
 
     return ret_port, ret_host
 
@@ -103,6 +108,5 @@ if __name__ == "__main__":
     print(simulated_subsystem)
 
     simulated_subsystem.start()
-
 
     print(f"Starting ADCS subsystem on port {port}")

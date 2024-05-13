0ff3597f11e4fc63fa403531541e561bfb0a43ce
waridh <waridh@proton.me>
2024-05-13 19:37:35 -0400

    updated the unit testing for ADCS

--- ADCS/abstract_interface.py
+++ ADCS/abstract_interface.py
@@ -1,12 +1,15 @@
 class ConnectionProtocol:
-    def send(self, data):
+    def send(self, data: bytes):
         """
-        This method will send data
+        This method will send data. Force the input to be bytes so that the
+        caller must format the data to be bytes already.
         """
         raise NotImplementedError
 
-    def recv(self):
+    def recv(self) -> bytes:
         """
         This method will recieve data. It should return it.
+
+        Fundamental assumption is that this will work with bytes
         """
         raise NotImplementedError

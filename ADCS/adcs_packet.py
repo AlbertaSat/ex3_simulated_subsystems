c609c2da0e515a9abf216e84f2c7c13593971b9a
waridh <waridh@proton.me>
2024-05-27 21:06:54 -0400

    completed more serialization and deserialization functions

--- ADCS/adcs_packet.py
+++ ADCS/adcs_packet.py
@@ -1,7 +1,10 @@
 from enum import Enum, auto
+from typing import Union
+
 
 class PacketType(Enum):
     """Specifying the packet type"""
+
     DATA = auto()
     CONTROL = auto()
 
@@ -13,11 +16,15 @@ class AdcsPacket:
         """TODO: Either make a class constructor or a factory object to create
         packets. This way, we ensure that only valid packets are created."""
         self.packet_type = None
-        self.data = None
+        self.data: Union[None, bytes] = None
 
     def to_bytes(self) -> bytes:
         """This function returns the bytes representation of the current packet."""
         raise NotImplementedError
+
+    @property
+    def data_length(self) -> int:
+        return 0 if self.data is None else len(self.data)
 
     @staticmethod
     def byte_of_type(packet_type: PacketType) -> bytes:
@@ -32,7 +39,20 @@ class AdcsPacket:
                 ret = b"\x00"
         return ret
 
+    @staticmethod
+    def type_of_byte(packet_byte: bytes) -> PacketType:
+        """Converts serial data back into a python enum"""
+        ret: PacketType
+        if len(packet_byte) != 1:
+            raise ValueError("type_of_byte", "Input not single byte")
+        match packet_byte:
+            case b"\x01":
+                ret = PacketType.DATA
+            case b"\x02":
+                ret = PacketType.CONTROL
+            case b"\x00":
+                raise RuntimeError("The generating end found an unhandled PacketType")
+            case _:
+                raise RuntimeError("Like error during transmission")
 
-if __name__ == "__main__":
-    unittest.main()
-    
+        return ret
--- ADCS/adcs_packet_test.py
+++ ADCS/adcs_packet_test.py
@@ -2,22 +2,40 @@ import unittest
 from unittest import TestCase
 from adcs_packet import AdcsPacket, PacketType
 
+
 class TestPacket(TestCase):
     def setUp(self):
         self.dut = AdcsPacket()
         return super().setUp()
 
     def tearDown(self):
-        self.dut = None
+        del self.dut
         return super().tearDown()
-
-    def test_testing_infrastructure(self):
-        self.assertTrue(True)
 
     def test_byte_of_type(self):
         self.assertEqual(self.dut.byte_of_type(PacketType.DATA), b"\x01")
         self.assertEqual(self.dut.byte_of_type(PacketType.CONTROL), b"\x02")
 
+    def test_type_of_byte(self):
+        test_eq = lambda x, y: self.assertEqual(self.dut.type_of_byte(x), y)
+
+        test_eq(b"\x01", PacketType.DATA)
+        test_eq(b"\x02", PacketType.CONTROL)
+
+        with self.assertRaises(RuntimeError):
+            self.dut.type_of_byte(b"\x00")
+
+        with self.assertRaises(RuntimeError):
+            self.dut.type_of_byte(b"\x77")
+
+        with self.assertRaises(ValueError):
+            self.dut.type_of_byte(b"\xDE\xAD\xBE\xEF")
+
+    def test_data_lenth(self):
+        """Testing if we can get the length field from a packet object"""
+        self.assertEqual(self.dut.data_length, 0)
+        self.dut.data = b"\xDE\xAD\xBE\xEF"
+        self.assertEqual(self.dut.data_length, 4)
 
 
 if __name__ == "__main__":

853165f29a637a829feb80f0d5a7806c5616e02f
waridh <waridh@proton.me>
2024-05-28 19:47:56 -0400

    Completed the basic packet serializer and deserializer

--- ADCS/adcs_packet.py
+++ ADCS/adcs_packet.py
@@ -13,23 +13,29 @@ class AdcsPacket:
     """This is the class representing the packet that the ADCS will do transaction with"""
 
     @classmethod
-    def from_bytes(cls):
+    def from_bytes(cls, input_stream: bytes):
         """Constructs an ADCS packet from bytes"""
-        raise NotImplementedError
+        ret = cls()
+        ret.packet_type = cls.type_of_byte(input_stream[0].to_bytes(1, "little"))
+        data_len = input_stream[1]
+        ret.data = input_stream[2 : (2 + data_len)]
+
+        return ret
 
     def __init__(self):
         """TODO: Either make a class constructor or a factory object to create
         packets. This way, we ensure that only valid packets are created."""
-        self.packet_type: Union[None, PacketType] = None
-        self.data: Union[None, bytes] = None
+        self.packet_type: None | PacketType = None
+        self.data: None | bytes = None
 
     def to_bytes(self) -> bytes:
         """This function returns the bytes representation of the current packet."""
+        if self.packet_type is None or self.data is None:
+            raise RuntimeError("The packet type cannot be None")
         byte_type = self.byte_of_type(self.packet_type)
-        byte_length = self.data_length.to_bytes(1, "little")
-        byte_data = self.byte_of_data(self.data)
+        byte_length = self.data_length.to_bytes(1, "little", signed=False)
 
-        return b"".join([byte_type, byte_length, byte_data])
+        return b"".join([byte_type, byte_length, self.data])
 
     @property
     def data_length(self) -> int:
@@ -68,10 +74,8 @@ class AdcsPacket:
                 raise RuntimeError("Like error during transmission")
         return ret
 
-    @staticmethod
-    def byte_of_data(data: Union[str, None]) -> bytes:
-        """Converts the data in form of string into byte using UTF-8 encoding.
-        UTF-8 allows for language agnostic communications."""
-        if data is None:
-            raise ValueError("Data value cannot be none")
-        return bytes(data, "utf-8")
+    def __eq__(self, value: object, /) -> bool:
+        if type(value) is type(self):
+            return value.packet_type == self.packet_type and value.data == self.data
+        else:
+            return False

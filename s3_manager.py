d5cda8428ab83fbd1d6b88e56e40f526b1ba012b
Jacques Fourie <jacques@fourie.ca>
2024-06-26 16:04:42 -0600

    Added simulated subsystem manager python file

---
+++ s3_manager.py
@@ -0,0 +1,26 @@
+"""This python program is the manger for the simulated satellite subsystems.
+
+Usage: s3_manager.py
+
+Copyright 2024 [Jacques Fourie]. Licensed under the Apache License, Version 2.0
+"""
+
+import sys
+import threading
+
+
+# pylint: disable=duplicate-code
+# no error
+__author__ = "Jacques Fourie"
+__copyright__ = """
+    Copyright 2023 [Jacques Fourie]
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License."""

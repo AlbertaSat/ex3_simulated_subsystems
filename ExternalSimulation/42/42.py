505edfb933e1dd6f483f6504f4d948f6eb0dac1d
Jacques Fourie <jacques@fourie.ca>
2024-06-26 23:02:12 -0600

    Updated 42 interface to use ExternalSimulationProvider

--- ExternalSimulation/42/42.py
+++ ExternalSimulation/42/42.py
@@ -9,21 +9,61 @@ path_to_script = path.dirname(path.abspath(__file__))
 path_to_42 = path.join(path_to_script, "..", "..", "..", "42", "42")
 path_to_42_setup = path_to_script + "/ex3"
 
+if __name__ == "__main__":
+    sys.path.append(path.dirname(path.dirname(path.dirname(path.abspath(__file__)))))
+
+from ExternalSimulation import ExternalSimulationProvider
+
 state_dictionary = {
-    'dfgm': {
-        "Bx": 0,
-        "By": 0,
-        "Bz": 0,
-    },
-    'daedalus': {
-        "temperature": 0,
+    'time': 0,
+    'magnetometer': [0, 0, 0], # [x, y, z]
+    'accelerometer': [0, 0, 0], # [x, y, z]
+    'gyroscope': [0, 0, 0], # [x, y, z]
+    'gps': {
+        "valid": 0,
+        "rollover": 0,
+        "week": 0,
+        "seconds": 0,
+        "PosN": [0, 0, 0],
+        "VelN": [0, 0, 0],
+        "PosW": [0, 0, 0],
+        "VelW": [0, 0, 0],
+        "Lng": 0,
+        "Lat": 0,
+        "Alt": 0,
+        "WgsLng": 0,
+        "WgsLat": 0,
+        "WgsAlt": 0,
     }
 }
 
-
 # Interface class for the 42 simulation
-class FTInterface:
-    def __init__(self, exec_path=path_to_42):
+class FortyTwo(ExternalSimulationProvider):
+    def __init__(self, launch_42=True, exec_path=path_to_42, host='localhost', tx_port=10001):
+
+        # Initialize 42 simulation
+        if launch_42:
+            self.launch_42(exec_path)
+        
+        # # Connect to localhost:10001 TCP/IP socket
+        print("Connecting to server")
+
+        count = 0
+        while True:
+            try:
+                self.tx_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                self.tx_socket.connect((host, tx_port))
+                break
+            except ConnectionRefusedError:
+                count += 1
+                if count > 10:
+                    print("42 Connection refused. Exiting")
+                print("42 Connection refused. Retrying in 1 second")
+                time.sleep(1)
+
+        super().__init__("42", state_dictionary)
+    
+    def launch_42(self, exec_path=path_to_42):
         print(f"Starting 42 simulation at {exec_path}")
         print(f"Using setup file at {path_to_42_setup}")
         # Launch the 42 simulation
@@ -32,46 +72,28 @@ class FTInterface:
         # Start the 42 simulation
         self.simulator = subprocess.Popen([exec_path, "../ex3_simulated_subsystems/ExternalSimulation/42/ex3"], cwd=exec_path[:-3])
 
-        # Initialize mutex and state dictionary for the FT
-        self.data = {}
-        self.state_mutex = Lock()
-        
-        # # Connect to localhost:10001 TCP/IP socket
-        print("Connecting to server")
-
-        count = 0
+    def update(self):
         while True:
-            try:
-                self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                self.s.connect(('localhost', 10001))
-                break
-            except ConnectionRefusedError:
-                count += 1
-                if count > 10:
-                    print("42 Connection refused. Exiting")
-                print("42 Connection refused. Retrying in 1 second")
-                time.sleep(1)
-        
-        # # Create a thread to listen for data from the server
-        # self.client_thread = Thread(target=self.thread_function)
+            self.update_state_atomic()
+            self.notify()
+            time.sleep(0.01)
 
     def update_state_atomic(self):
         """ Function to read the state of the 42 simulation
         """
         with self.state_mutex:
             # Socket state data from the simulation starts with `TIME: XXXXXXXXXXX` and ends with `[EOF]`
-            
-            # Read full message from the simulation
-            data = self.s.recv(1024*128).decode("utf-8")
-            
-            print(f"Received: {data}")
-            
-            # Structure
+            # Example:
             # TIME 0.0000000000000000
             # DictionaryList[0].Name = x[ y[ z]]
             # DictionaryList[1].Name = x[ y[ z]]
             # ...
             # [EOF]
+            
+            # Read message from the simulation
+            data = self.tx_socket.recv(1024*128).decode("utf-8")
+            
+            print(f"Received: {data}")
             
             # Split the data into lines
             lines = data.split("\n")
@@ -92,7 +114,7 @@ class FTInterface:
                     i = int(line_data[0].split("[")[1].split("]")[0])
                     values = line_data[1].split(" ")
                     
-                    # CHeck if the name is in the dictionary
+                    # Check if the name is in the dictionary
                     if name not in self.data:
                         self.data[name] = [{}]
                     
@@ -105,25 +127,15 @@ class FTInterface:
                         self.data[name][i][field] = []
                     
                     self.data[name][i][field] = values
-  
-    def thread_function(self):
-        """ Function to listen for data from the server and process it
-        """
-        while True:
-            data = self.s.recv(1024)
-            print(f"Received: {data}")
 
-    # def __del__(self):
-    #     if self.s is not None:
-    #         self.s.close()
-    #     if self.simulator is not None:
-    #         self.simulator.terminate()
+    def __del__(self):
+        if self.tx_socket is not None:
+            self.tx_socket.close()
+        if self.simulator is not None:
+            self.simulator.terminate()
 
         
         
 if __name__ == "__main__":
-    ft_interface = FTInterface()
-    ft_interface.update_state_atomic()
-    while True:
-        pass
-    # ft_interface.client_thread.start()
+    ft_interface = FortyTwo()
+    ft_interface.start()

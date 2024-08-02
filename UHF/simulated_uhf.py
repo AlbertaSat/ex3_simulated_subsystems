a97e97240542100ce9779e4479eb8bdc0d3c9d7a
Drake <dboul63@gmail.com>
2024-08-02 10:54:52 -0600

    Added functionality for clients to reconnect to server after disconnecting

--- UHF/simulated_uhf.py
+++ UHF/simulated_uhf.py
@@ -11,22 +11,29 @@ import threading
 import time
 import select
 
-DEBUG = 0
+DEBUG = 1
 BUFF_SIZE = 128
 COMMS_SIDE_SERVER_PORT = 1234
 GS_SIDE_SERVER_PORT = 1235
-COMM_SERVER_HOSTNAME = socket.gethostname()
-GS_SERVER_HOSTNAME = socket.gethostname()
+COMM_SERVER_HOSTNAME = '127.0.0.1'
+GS_SERVER_HOSTNAME = '127.0.0.1'
 
 # Use lists for shared buffers to allow modification within threads
 gs_data = []
 comm_data = []
 
+# Using mutable type as a makeshift pointer so clients can be modified across threads
+client_pointer = {}
+
+# client keys
+GS_CLIENT_KEY = "gs_client"
+COMM_CLIENT_KEY = "comm_client"
+
 # Disable global statements and invalid names for beacon_string variable
 # pylint: disable=global-statement
 BEACON_STRING = "beacon"
 
-# Delimtter for command
+# Delimiter for command
 DELIMITER = ':'
 
 
@@ -69,31 +76,43 @@ def mod_beacon(message, lock):
     except TypeError as e:
         print(f"Failed to parse: {e}")
 
+    except IndexError as e:
+        print(f"Improper us of Delimiter, Failed to parse: {e}")
 
-def beacon(client, lock):
+
+def beacon(client_key, server, lock, n=5):
     """
     continually sends beacon every n seconds
 
     Args:
-        client(socket): the client socket communicating to the ground station
+        client_key(string): key to access the dictionary where client socket object 
+        is stored (client_pointer)
         lock(mutex): the lock used when interacting with client socket
-
+        n(integer): the amount of time in seconds between beacon messages
+            defult value of 5.
     Returns:
         None
     """
 
     while True:
-        # Send beacon every 5 seconds for testing
-        time.sleep(5)
+        time.sleep(n)
         with lock:
             try:
-                client.sendall(bytes(BEACON_STRING, "utf-8"))
+                client_pointer[client_key].sendall(bytes(BEACON_STRING, "utf-8"))
                 if DEBUG:
                     print("Sent beacon")
 
-            except (BrokenPipeError, KeyboardInterrupt) as e:
+            except BrokenPipeError as e:
+                print(f"Error sending beacon: {e}")
+                print("waiting for client to connect")
+                client_pointer[client_key].close()
+                client_pointer[client_key] = search_client(server, lock)
+
+            except KeyboardInterrupt as e:
                 print(f"Error sending beacon: {e}")
                 break
+
+
 
 
 def start_server(hostname, port):
@@ -109,12 +128,13 @@ def start_server(hostname, port):
     """
 
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     server.bind((hostname, port))
     server.listen(5)
     return server
 
 
-def search_client(server):
+def search_client(server, lock):
     """
     Waits for client to connect to the servver
 
@@ -125,60 +145,68 @@ def search_client(server):
         client(socket): client socket object
         
     """
+    print("This ran")
+    with lock:
+        try:
+            client, addr = server.accept()
+            print("Connected to", addr)
+            return client
 
-    try:
-        client, addr = server.accept()
-        print("Connected to", addr)
-        return client
-
-    except (BrokenPipeError, KeyboardInterrupt, OSError) as e:
-        server.close()
-        print(f"Error occurred: {e}")
-
-    return None
+        except (BrokenPipeError, OSError) as e:
+            print(f"Error occurred: {e}")
+            return None
 
 
-def send_msg(client, buffer, lock):
+
+def send_msg(client_key, buffer, lock):
     """
     sends message from buffer via client socket 
 
     Args:
-        client(socket): client socket object that sends the message
+        client_key(string): key to access the dictionary where client socket object 
+        is stored (client_pointer)
         buffer(list): shared buffer containing message to be sent
         lock(mutex): lock to prevent a race condition on shared data
-    
+
     Returns:
         None
     """
 
     while True:
-        with lock:
-            if buffer:
-                if DEBUG:
-                    print("update:")
-                    print(gs_data)
-                    print(comm_data)
-
-                try:
-                    message = buffer.pop(0)
-                    client.sendall(message)
+        try:
+            with lock:
+                if buffer:
                     if DEBUG:
+                        print("update:")
+                        print("***** BEFORE *****")
+                        print(buffer)
+                        print("******************")
+
+                    message = buffer.pop(0)
+                    client_pointer[client_key].sendall(message)
+
+                    if DEBUG:
+                        print("***** AFTER *****")
                         print(f"Sent {message}")
-                        print(gs_data)
-                        print(comm_data)
-
-                except (BrokenPipeError, KeyboardInterrupt) as e:
-                    print(f"Error sending message: {e}")
-                    client.close()
-                    break
+                        print(buffer)
+                        print("******************")
 
 
-def receive_msg(client, buffer, lock):
+        except KeyboardInterrupt as e:
+            print(f"Error sending message: {e}")
+            break
+
+        except BrokenPipeError as e:
+            print(f"Pipe Broke: {e}")
+
+
+def receive_msg(client_key, server, buffer, lock):
     """
     listens for messages on the client and stores them in the buffer
 
     Args: 
-        client(socket): client socket object that receives the message
+        client_key(string): key to access the dictionary where client socket object 
+        is stored (client_pointer)
         buffer(list): shared buffer used to store received messsages
         lock(mutex): lock to prevent a race condition on shared data
 
@@ -189,16 +217,16 @@ def receive_msg(client, buffer, lock):
     while True:
         try:
             # Use select to check if data is available to be read
-            ready_to_read, _, _ = select.select([client], [], [], 0.5)  # timeout set to 0.5 seconds
+            ready_to_read, _, _ = select.select([client_pointer[client_key]], [], [], 0.5)
 
             if ready_to_read:
-                message = client.recv(BUFF_SIZE)
+                message = client_pointer[client_key].recv(BUFF_SIZE)
 
                 if message:
                     if DEBUG:
                         print(f"Received: {message.decode('utf-8')}")
 
-                    _, port = client.getsockname()
+                    _, port = client_pointer[client_key].getsockname()
 
                     # Primitive check to see if messages intent was to modify UHF params
                     if port == COMMS_SIDE_SERVER_PORT and b"MOD_UHF" in message:
@@ -207,18 +235,24 @@ def receive_msg(client, buffer, lock):
                     else:
                         with lock:
                             buffer.append(message)
-
                 else:
-                    print("Connection closed by the client")
-                    break
+                    client_pointer[client_key].close()
+                    client_pointer[client_key] = search_client(server, lock)
 
         except BlockingIOError:
             pass
 
-        except (BrokenPipeError, KeyboardInterrupt) as e:
+        except KeyboardInterrupt as e:
             print(f"Error receiving message: {e}")
-            client.close()
+            client_pointer[client_key].close()
             break
+
+        except BrokenPipeError as e:
+            print(f"Pipe Broke: {e}")
+            print("waiting for client to connect")
+            client_pointer[client_key].close()
+            client_pointer[client_key] = search_client(server, lock)
+
 
 
 def main():
@@ -233,16 +267,27 @@ def main():
     comm_server = start_server(COMM_SERVER_HOSTNAME, COMMS_SIDE_SERVER_PORT)
     gs_server = start_server(GS_SERVER_HOSTNAME, GS_SIDE_SERVER_PORT)
 
-    comm_client = search_client(comm_server)
-    gs_client = search_client(gs_server)
-
     client_lock = threading.Lock()
 
-    comm_listen = threading.Thread(target=receive_msg, args=(comm_client, gs_data, client_lock))
-    gs_listen = threading.Thread(target=receive_msg, args=(gs_client, comm_data, client_lock))
 
-    comm_send = threading.Thread(target=send_msg, args=(comm_client, comm_data, client_lock))
-    gs_send = threading.Thread(target=send_msg, args=(gs_client, gs_data, client_lock))
+    comm_client = search_client(comm_server, client_lock)
+    client_pointer[COMM_CLIENT_KEY] = comm_client
+    gs_client = search_client(gs_server, client_lock)
+    client_pointer[GS_CLIENT_KEY] = gs_client
+
+    if comm_client is None or gs_client is None:
+        print("Failed to connect to client")
+        return -1
+
+    comm_listen = threading.Thread(target=receive_msg,
+                                args=(COMM_CLIENT_KEY, comm_server, gs_data, client_lock))
+    gs_listen = threading.Thread(target=receive_msg,
+                                args=(GS_CLIENT_KEY, gs_server, comm_data, client_lock))
+
+    comm_send = threading.Thread(target=send_msg,
+                                args=(COMM_CLIENT_KEY, comm_data, client_lock))
+    gs_send = threading.Thread(target=send_msg,
+                                args=(GS_CLIENT_KEY, gs_data, client_lock))
 
     comm_listen.start()
     gs_listen.start()
@@ -250,8 +295,10 @@ def main():
     comm_send.start()
     gs_send.start()
 
-    beacon(gs_client, client_lock)
+    beacon(GS_CLIENT_KEY, gs_server, client_lock)
 
+    comm_server.close()
+    gs_server.close()
 
 
 main()

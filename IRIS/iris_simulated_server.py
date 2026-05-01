e3ab0a5930ef8fab2b2909b51f49aeece9e16f55
Drake Boulianne <dboul63@gmail.com>
2026-04-30 20:41:12 -0700

    more pylint fixes

--- IRIS/iris_simulated_server.py
+++ IRIS/iris_simulated_server.py
@@ -185,4 +185,3 @@ __copyright__ = """
     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
     See the License for the specific language governing permissions and
     limitations under the License."""
-
